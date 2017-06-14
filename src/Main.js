bce9ec33e6111e27369ce14051d6cc1e7e2df91a
skylerkolli <skylerkolli@gmail.com>
2017-06-14 16:41:53 -0400

    noteherder

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './Main.css'
+import Sidebar from './Sidebar'
+import NoteList from './NoteList'
+import NoteForm from './NoteForm'
+
+const Main = () => {
+  return (
+    <div className="Main">
+      <Sidebar />
+      <NoteList />
+      <NoteForm />
+    </div>
+  )
+}
+
+export default Main
---
+++ src/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './Sidebar.css'
+import quill from './quill.svg'
+import newHover from './new-hover.png'
+import newIcon from './new.png'
+
+const Sidebar = () => {
+  return (
+    <nav className="Sidebar">
+      <div className="logo">
+        <img src={quill} alt="Noteherder" />
+      </div>
+      <button className="new-note">
+        <img src={newHover} alt="New note" />
+        <img className="outline" src={newIcon} alt="New note" />
+      </button>
+    </nav>
+  )
+}
+
+export default Sidebar
